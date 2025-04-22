5d2d68faba2f42984c2b018ae0990466bb458f66
GabrielHenriqueHE <gabriel.evaristo.2004@gmail.com>
2025-04-22 11:44:52 -0300

    feat: renderizando informações hardcoded em tela

--- galeria/views.py
+++ galeria/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'galeria/index.html')
+
+    dados = {
+    1: {
+        'nome': 'Nebulosa de Carina',
+        'legenda': 'https://webbtelescope.org',
+        'reproducao': 'James Webb Space Telescope/NASA',
+    },
+    2: {
+        'nome': 'Galáxia NGC 1079',
+        'legenda': 'https://nasa.gov',
+        'reproducao': 'Hubble Space Telescope/NASA',
+    }
+}
+    
+    return render(request, 'galeria/index.html', {'cards': dados})
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')

775736bc03c4b95f9484934c03495813a9fc22f7
GabrielHenriqueHE <gabriel.evaristo.2004@gmail.com>
2025-04-23 15:28:49 -0300

    feat: criando os formulários de cadastro e login

---
+++ usuarios/forms.py
@@ -0,0 +1,63 @@
+from django import forms
+
+class LoginForm(forms.Form):
+    nome_login = forms.CharField(
+        label='Nome de Login',
+        required=True,
+        max_length=100,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Ex.: João Silva'
+        })
+    )
+
+    senha = forms.CharField(
+        label='Senha',
+        required=True,
+        max_length=70,
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Digite sua senha'
+        })        
+    )
+
+class CadastroForm(forms.Form):
+    nome_completo = forms.CharField(
+        label='Nome completo',
+        required=True,
+        max_length=100,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Ex.: João Silva'
+        })
+    )
+
+    email = forms.EmailField(
+        label='E-mail',
+        required=True,
+        max_length=100,
+        widget=forms.EmailInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Ex.: joaosilva@xpto.com'
+        })
+    )
+
+    senha = forms.CharField(
+        label='Senha',
+        required=True,
+        max_length=70,
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Digite sua senha'
+        })        
+    )
+
+    confirma_senha = forms.CharField(
+        label='Confirmação de senha',
+        required=True,
+        max_length=70,
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Digite sua senha novamente'
+        })        
+    )
